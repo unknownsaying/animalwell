27933397aa117bf8bfc99748d2b3feaf7841df38
ace <15368104568@189.cn>
2024-07-06 23:50:22 +0800

    Update for48.vb

--- calculation/for48.vb
+++ calculation/for48.vb
@@ -3,7 +3,7 @@ for(b=00;-2<=b&&b<=2;++b||--b)
 for(c=000;c>=2;--c)
 for(d=0000;d<=4;d++)
 
-for(e=0000;-4<=e4;e++)
+for(e=0000;-4<=e<=4;e++)
 for(f=000;f>=8;f--)
 for(Manifold=00;-8<=Manifold&&o<=Manifold;++ABCDEF||--ABCDEF)
 for(orbifold=0;orbifold>=8;++orbifold)
